534a5445eddfa68ad5fc84893b977bab6af8b1ae
ayukoyamashita <ayuko.yamashita@kufusumai.co.jp>
2023-12-23 12:12:28 +0900

    修正

--- components/Result.tsx
+++ components/Result.tsx
@@ -3,9 +3,38 @@ import * as React from "react";
 import THistory from "../types/THistory";
 import {useEffect, useState} from "react";
 
+import Score0Image from "../images/score0.png";
+import Score10Image from "../images/score10.png";
+import Score20Image from "../images/score20.png";
+import Score30Image from "../images/score30.png";
+import Score40Image from "../images/score40.png";
+import Score50Image from "../images/score50.png";
+import Score60Image from "../images/score60.png";
+import Score70Image from "../images/score70.png";
+import Score80Image from "../images/score80.png";
+import Score90Image from "../images/score90.png";
+import Score100Image from "../images/score100.png";
+
+import commentVeryGoodImage from "../images/comment_verygood.png";
+import commentGoodImage from "../images/comment_good.png";
+import commentFightImage from "../images/comment_fight.png";
+
 const Result = () => {
   const [history, setHistory] = useState([])
   let [correctNum, setCorrectNum] = useState(0);
+  const scoreImages = [
+    Score0Image,
+    Score10Image,
+    Score20Image,
+    Score30Image,
+    Score40Image,
+    Score50Image,
+    Score60Image,
+    Score70Image,
+    Score80Image,
+    Score90Image,
+    Score100Image,
+  ];
 
   useEffect(() => {
     const result = localStorage.getItem('result');
@@ -16,11 +45,26 @@ const Result = () => {
     }
   }, []);
 
+  const commentImage = () => {
+    if (correctNum === 10) {
+      return commentVeryGoodImage;
+    } else if (5 <= correctNum && correctNum < 10) {
+      return commentGoodImage;
+    } else {
+      return commentFightImage;
+    }
+  }
+
   return (
     <main className="lg:container p-6 mx-auto text-center">
       {history.length > 0 && (
         <>
-          <p className="mb-10">{correctNum * 10}点</p>
+          <p className="mt-10">
+            <img src={scoreImages[correctNum].src} alt={`${correctNum * 10}点`} className="m-auto h-56" />
+          </p>
+          <p className="mb-10">
+            <img src={commentImage().src} alt="とてもいいね" className="m-auto h-16" />
+          </p>
 
           <ol className="inline-block">
             {history.map((his: THistory, i: number) => (
@@ -47,7 +91,7 @@ const Result = () => {
       )}
 
       <div>
-        <Button href="/" as={Link} color="primary" size="lg">もういちどチャレンジ</Button>
+        <Button href="../" as={Link} color="primary" size="lg">もういちどチャレンジ</Button>
       </div>
     </main>
   )
